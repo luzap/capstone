88200f91f9fa270d85bfbf534f812502646e65bf
Lukas Zapolskas <lz1477@nyu.edu>
2020-04-21 23:18:11 +0400

    Working on controls for vehicle

--- sim/vehicle.py
+++ sim/vehicle.py
@@ -1,6 +1,13 @@
 #! /usr/bin/python3
 import numpy as np
+from typing import List
 from collections import namedtuple
+
+class Actions:
+    STOP = 0
+    SLOW = 1
+    SPEED = 2
+    TURN = 3
 
 action = namedtuple("action", "name delta time")
 
@@ -20,7 +27,6 @@ class Vehicle:
         self.controls = []
 
     def move(self):
-        # TODO See what the default heading here is 
         self.state += np.array([self.vel * np.cos(self.state[2]) * self.dt,
                                 self.vel * np.sin(self.state[2]) * self.dt,
                                 0.0])
@@ -34,11 +40,21 @@ class Vehicle:
                                    ])
         print(self.state)
 
-    # Control format: ACTION:TIME
-    def control(self, controls):
-        pass
+    # Control format: ACTION:AMOUNT:TIME
+    def control(self, controls: List[str]):
+        for con in controls:
+            parts = con.strip().split(":")
+            self.controls.append(action(
+                getattr(Actions, parts[0]),
+                float(parts[1]), float(parts[2])))
+        # Making sure that the first action comes first
+        self.controls.sort(key=lambda con: con[2])
 
     def __next__(self):
+        # TODO Check for current command and time
+        # TODO Figure out how much turning needs to be done
+        #
+
         pass
 
 
