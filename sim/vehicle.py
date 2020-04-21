9b5e7c975b3878fd180bc64d693f028f03267d7d
Lukas Zapolskas <lz1477@nyu.edu>
2020-04-21 23:07:37 +0400

    Basic vehicular motion in 2D

--- sim/vehicle.py
+++ sim/vehicle.py
@@ -1,17 +1,18 @@
 #! /usr/bin/python3
 import numpy as np
-from collection import namedtuple
-
-__X = 0
-__Y = 1
-__BEARING = 2
+from collections import namedtuple
 
 action = namedtuple("action", "name delta time")
 
 class Vehicle:
 
-    def __init__(self, x: fl, y, dt = 0.01, vel=1, ang_vel=0, bearing=0):
-        self.state = np.array([x, y, bearing])
+    __X = 0
+    __Y = 1
+    __BEARING = 2
+
+    def __init__(self, x: float, y: float, dt = 0.01, vel=1.0, ang_vel=0,
+                 bearing=0.0):
+        self.state = np.array([float(x), float(y), float(bearing)])
         self.vel = vel
         self.ang_vel = ang_vel
         self.dt = dt
@@ -20,16 +21,18 @@ class Vehicle:
 
     def move(self):
         # TODO See what the default heading here is 
-        self.state += np.array([-self.vel * np.sin(self.state[__BEARING]),
-                                self.vel * np.cos(self.state[__BEARING]),
-                                0])
-        if self.ang_vel:
+        self.state += np.array([self.vel * np.cos(self.state[2]) * self.dt,
+                                self.vel * np.sin(self.state[2]) * self.dt,
+                                0.0])
+        if self.ang_vel > 0.001:
+            print('this procs')
             r = abs(self.vel / self.ang_vel)
             dtheta = self.ang_vel * self.dt
-            self.state += np.array([r * np.sin(self.state[__BEARING] + dtheta),
-                                    r * np.cos(self.state + dtheta),
+            self.state += np.array([r * np.cos(self.state[2] + dtheta) * self.dt,
+                                    r * np.sin(self.state + dtheta) * self.dt,
                                     dtheta
                                    ])
+        print(self.state)
 
     # Control format: ACTION:TIME
     def control(self, controls):
