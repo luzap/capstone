48f912aebe8df248b6945c8936318a65a63337c9
Lukas Zapolskas <lz1477@nyu.edu>
2020-05-05 15:01:16 +0400

    Preparing for final stages

--- sim/drivers.py
+++ sim/drivers.py
@@ -2,10 +2,18 @@
 import pandas as pd
 import numpy as np
 
+# TODO Normalize the column data to something more appropriate for lat, long, alt\n,
+# TODO Merge the GPS and pose data on the timestep, should that be the case\n,
+# TODO Start pushing this through the Kalman filter and see the residuals\n,
+# TODO If the residuals are fine, then we move on
+
 def read_data():
     gps = pd.read_csv("../data/AGZ_subset/Log Files/OnboardGPS.csv",
                       skipinitialspace=True)
     pose = pd.read_csv("../data/AGZ_subset/Log Files/OnboardPose.csv",
                        skipinitialspace=True)
 
-    gps.drop([], axis=1, inplace=True)
+    gps.drop(["imgid", "Unnamed: 14", "Unnamed: 15", "Unnamed: 16",
+              "Unnamed: 17", "num_sat", "fix_type", "eph_m", "epv_m"],
+             axis=1, inplace=True)
+
--- sim/error.py
+++ sim/error.py
@@ -1,2 +1,21 @@
 #! /usr/bin/python3
 import numpy as np
+from scipy.stats import rayleigh, maxwell
+
+# TODO Four cases: p=2 (Rayleigh, uncorrelated, simple case)
+#                   p=2 correlated can be treated manually,
+#                   p=3 (Maxwell Boltzmann)
+#                   p=3 (different sigmas)
+# TODO Overlap
+
+def rayleigh_2d_errors(mu_1, sigma_1, mu_2, sigma_2):
+    pass
+
+def uncorrelated_3d_errors(mu_1, sigma_1, mu_2, sigma_2):
+    pass
+
+def uncorrelated_2d_errors(mu_1, sigma_1, mu_2, sigma_2):
+    pass
+
+def correlated_3d_errors(mu_1, sigma_1, mu_2, sigma_2):
+    pass
