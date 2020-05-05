95b3b8b7b95e5a612bf9341e58a26ca690354762
Lukas Zapolskas <lz1477@nyu.edu>
2020-05-05 21:41:06 +0400

    Working on curve fitting

--- sim/error.py
+++ sim/error.py
@@ -1,21 +1,30 @@
 #! /usr/bin/python3
 import numpy as np
-from scipy.stats import rayleigh, maxwell
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
 
-# TODO Four cases: p=2 (Rayleigh, uncorrelated, simple case)
-#                   p=2 correlated can be treated manually,
-#                   p=3 (Maxwell Boltzmann)
-#                   p=3 (different sigmas)
-# TODO Overlap
+def exponential(x, mu, sigma, height):
+    return height * np.exp(-(x - mu) ** 2/(sigma ** 2))
 
-def rayleigh_2d_errors(mu_1, sigma_1, mu_2, sigma_2):
-    pass
+def fit_distribution(mu, Sigma, best_fit=exponential, sample_num=10000):
+    sums = np.zeros(sample_num)
+    for i, _ in enumerate(sums):
+        sample = np.random.multivariate_normal(mu, Sigma)
+        sums[i] = np.dot(sample, sample.T)
 
-def uncorrelated_3d_errors(mu_1, sigma_1, mu_2, sigma_2):
-    pass
+    # We classify the entire thing via histograms, take their centerpoints
+    # and use those centerpoints to fit a curve
+    bin_heights, bin_borders = np.histogram(sums, bins="auto")
+    bin_widths = np.diff(bin_borders)
+    bin_centers = bin_borders[:-1] + bin_widths/2
 
-def uncorrelated_2d_errors(mu_1, sigma_1, mu_2, sigma_2):
-    pass
+    best_fit_coeffs, _ = curve_fit(best_fit, bin_centers, bin_heights, maxvfev=1000)
+    x_width = np.linspace(bin_borders[0], bin_borders[-1], sample_num)
+    plt.bar(bin_centers, bin_heights, width=bin_widths, label="histogram")
+    plt.plot(x_width, exponential(x_width, *best_fit_coeffs), label="fit", c="red")
+    plt.legend()
+    plt.show()
+    print(best_fit_coeffs)
 
-def correlated_3d_errors(mu_1, sigma_1, mu_2, sigma_2):
-    pass
+if __name__ == "__main__":
+    fit_distribution(np.array([0, 0]), np.array([[1, 0],[0, 1]]), sample_num=1000)
--- sim/vehicle.py
+++ sim/vehicle.py
@@ -5,6 +5,7 @@ import matplotlib.animation
 from typing import List, Callable
 from collections import namedtuple
 
+# TODO At least test this today with the Kalman filter
 def vehicle_2d_process_model(x0, dt, vel: float, ang_vel: float, use_noise: bool
                              = False):
         # TODO Figure out sensible error values based on the velocity
@@ -18,46 +19,6 @@ def vehicle_2d_process_model(x0, dt, vel: float, ang_vel: float, use_noise: bool
 
         return x0 + translational_velocity * dt + angular_velocity * dt
 
-
-def animate_2d(length: int,
-               vel: Callable[[float], float], ang_vel:
-               Callable[[float], float]):
-    # TODO Need plot for position and angle, then for residuals of angle
-    # and residuals of position
-    fig, axes = plt.subplots(2, 2)
-    axes[0].grid()
-
-    # TODO Initialize random start positions w/
-
-
-    # # ax.plot returns a list, which is why the destructuring is necessary
-    # veh_line = axes[0].plot(v.state[0], v.state[1], 'o-', lw=2)[0]
-    # ang_line =  axes[1].plot(v.state[2], v.current_time, 'b-', lw=2)[0]
-
-    # for axis in axes:
-    #     axis.set_xlim(0, length)
-    # # TODO Add axes names and plot names
-    # axes[1].set_ylim(0, 2*np.pi)
-
-    # veh_path = np.zeros((length * 100, 2))
-    # ang_path = np.zeros((length * 100, 2))
-
-    # def animate(frame):
-    #     next(v)
-    #     veh_path[frame] = v.state[0:2]
-    #     ang_path[frame] = np.array([v.current_time, v.state[2]])
-
-    #     veh_line.set_data(veh_path[:, 0], veh_path[:, 1])
-    #     ang_line.set_data(ang_path[:, 0], ang_path[:, 1])
-    #     return veh_line, ang_line
-
-    # # TODO Add optional delay parameter
-    # _ = matplotlib.animation.FuncAnimation(fig, animate, frames=length*100,
-    #                                        interval=0.1, repeat=False, blit=True)
-    # plt.show()
-
-
-
 def vehicle_3d_process_model(x, dt, vel, yaw, pitch, roll):
     dx = vel * np.array([np.cos(x[3]) * np.cos(x[4]),
                          np.sin(x[3]) * np.cos(x[4]),
@@ -67,25 +28,3 @@ def vehicle_3d_process_model(x, dt, vel, yaw, pitch, roll):
     dalpha = roll * np.array([0, 0, 0, 0, 0, 1])
 
     return (dx + dtheta + dbeta + dalpha) * dt
-
-
-
-
-class Vehicle3D:
-
-    def __init__(self, x: float, y: float, z: float, theta: float, beta: float,
-                 alpha: float, vel: Callable[[float], float],
-                 yaw: Callable[[float], float], pitch: Callable[[float], float],
-                 roll: Callable[[float], float], dt: float = 0.01):
-        self.state = np.array([x, y, z, theta, beta, alpha])
-        self.current_time = 0
-        self.dt = dt
-        self.vel = vel
-        self.yaw = yaw
-        self.pitch = pitch
-        self.roll = roll
-
-
-    def __next__(self):
-        self.__move()
-        self.current_time += self.dt
